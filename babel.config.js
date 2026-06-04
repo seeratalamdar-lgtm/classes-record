4ca29a45592e9be911f7e352a5114e22851c058b
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-04 07:35:01 +0000

    Fix: remove web module aliases from babel.config.js - fixes ALL upload/download/sharing issues on Android

--- babel.config.js
+++ babel.config.js
@@ -7,15 +7,6 @@ module.exports = function(api) {
         'module-resolver',
         {
           alias: {
-            'expo-sharing': './lib/sharing-web',
-            'expo-file-system': './lib/filesystem-web',
-            'expo-document-picker': './lib/document-picker-web',
-            'expo-image-picker': './lib/image-picker-web',
-            'expo-location': './lib/location-web',
-            'expo-print': './lib/print-web',
-            'expo-haptics': './lib/haptics-web',
-            'expo-screen-orientation': './lib/orientation-web',
-            '@react-native-async-storage/async-storage': './lib/webStorage',
             'react-native-keyboard-controller': 'react-native'
           }
         }
