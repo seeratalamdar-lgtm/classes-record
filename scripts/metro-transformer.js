356ecb272b1a05ce15d50d27262b2b493321003e
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-05 11:55:27 +0000

    Fix: use correct @expo/metro-config/babel-transformer path in Metro transformer

--- scripts/metro-transformer.js
+++ scripts/metro-transformer.js
@@ -1,7 +1,6 @@
-const upstreamTransformer = require('@expo/metro-config/build/transformer/default-transformer');
+const upstreamTransformer = require('@expo/metro-config/babel-transformer');
 
 module.exports.transform = async function(params) {
-  // Replace private class fields with underscore equivalents before Babel processes them
   if (params.filename && params.filename.includes('node_modules/react-native')) {
     if (params.src && params.src.includes('#')) {
       params = {
