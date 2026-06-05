53f04e49cb16992162f8f7a1464dfdea0caabfd5
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-05 17:19:44 +0000

    Fix: switch Android JS engine from Hermes to JSC to avoid private class field errors

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,2 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add custom transformer to handle private class fields
-const originalTransformerPath = config.transformer?.babelTransformerPath;
-
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('./scripts/metro-transformer.js'),
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
