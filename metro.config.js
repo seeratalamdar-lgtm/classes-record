862d96fdd7ede35eac493ed28e0c9a55998c77b2
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-05 05:30:45 +0000

    Fix: configure Metro to transpile react-native private class fields

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Force Babel to transform react-native private class fields (DOMRectReadOnly etc.)
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+
+config.resolver = {
+  ...config.resolver,
+  unstable_enablePackageExports: false,
+};
+
+// Ensure react-native internal files with private class fields are transpiled
+const originalTransformIgnore = config.transformer?.transformIgnorePatterns || [];
+config.transformer.transformIgnorePatterns = [
+  'node_modules/(?!(react-native|@react-native|expo|@expo|@unimodules|unimodules|react-navigation|@react-navigation|react-native-svg|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-keyboard-controller|@react-native-community|react-native-worklets)/).*',
+];
+
+module.exports = config;
