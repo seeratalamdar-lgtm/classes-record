cc9ff4f2bb3ca1c69a7cbd392e2645f99b014a72
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-05 10:45:18 +0000

    Fix: Babel transform private class fields - remove non-existent plugin

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,6 @@ module.exports = function(api) {
       ],
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-field-declarations', { loose: true }],
     ]
   };
 };
