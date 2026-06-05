2bcc5bcb459616bd94841e98ec80df78d1b580a3
Alamdar Hussain <alamdar.hussain@seecs.edu.pk>
2026-06-05 10:44:51 +0000

    Fix: add Babel transforms for private class fields - Hermes compatibility

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,10 @@ module.exports = function(api) {
             'react-native-keyboard-controller': 'react-native'
           }
         }
-      ]
+      ],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-field-declarations', { loose: true }],
     ]
   };
 };
